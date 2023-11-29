e82f118ae6e335955924fb8c771d926e3bbe791d
stephenyu <s.yu@less3.com>
2023-11-29 18:13:01 +0000

    Able to Select the Start Day.

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,11 @@
-import { Button, FormField, Select, Rows, Text } from "@canva/app-ui-kit";
+import { Button, FormField, Select, Rows, RadioGroup } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
 import * as React from "react";
 import styles from "styles/components.css";
 
 const listOfMonths = (() => {
-  const date = new Date();
-  date.setMonth(0);
-
-  const monthNames: Array<string> = [];
-
-  const options = {
-    month: "long",
-  } as Intl.DateTimeFormatOptions;
-
-  for (let i = 0; i < 12; i++) {
-    const monthHeading = new Intl.DateTimeFormat("en-US", options).format(date);
-    monthNames.push(monthHeading);
-    date.setMonth(date.getMonth() + 1);
-  }
-
-  return monthNames;
+// todo: I need to localise this
+return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 })();
 
 const MonthSelect = ({
@@ -46,9 +32,9 @@ const YearSelect = ({
 }) => {
   const options: Array<{ value: string; label: string }> = [];
   const date = new Date();
-  const previousYear = date.getFullYear() - 1;
+  const previousYear = date.getFullYear() - 10;
 
-  for (let i = previousYear; i < previousYear + 5; i++) {
+  for (let i = previousYear; i < date.getFullYear() + 10; i++) {
     options.push({ value: i.toString(), label: i.toString() });
   }
 
@@ -58,8 +44,10 @@ const YearSelect = ({
 const doubleSpaceNumber = (dateString: string) =>
   dateString.length == 1 ? " " + dateString : dateString;
 
-function getMonth(monthNumeric: number, yearNumeric: number) {
+function getMonth(monthNumeric: number, yearNumeric: number, startDay: "sun"|"mon") {
   const date = new Date();
+
+  console.log(startDay);
 
   date.setMonth(monthNumeric);
   date.setFullYear(yearNumeric);
@@ -71,22 +59,33 @@ function getMonth(monthNumeric: number, yearNumeric: number) {
   } as Intl.DateTimeFormatOptions;
 
   const monthHeading = new Intl.DateTimeFormat("en-US", options).format(date);
+  let weekOfDates: Array<string> = [];
 
   const currentMonth = date.getMonth();
 
-  const month: Array<string> = [monthHeading, "Su Mo Tu We Th Fr Sa"];
-  let weekOfDates: Array<string> = [];
+  const month: Array<string> = (startDay === "sun")
+    ? [monthHeading, "Su Mo Tu We Th Fr Sa"]
+    : [monthHeading, "Mo Tu We Th Fr Sa Su"];
 
-  for (let i = date.getDay(); i > 0; i--) {
-    weekOfDates.push("  ");
+  if (startDay === "sun") {
+      for (let i = date.getDay(); i > 0; i--) {
+          weekOfDates.push("  ");
+      }
+  } else {
+      const count = (date.getDay() === 0) ? 6 : date.getDay() - 1;
+      for (let i = 0; i < count; i++) {
+          weekOfDates.push("  ");
+      }
   }
+
+  const startOfTheWeekDay = (startDay === "sun") ? 0 : 1;
 
   while (date.getMonth() === currentMonth) {
     const numericalDate = date.getDate();
     weekOfDates.push(doubleSpaceNumber(numericalDate.toString()));
 
     date.setDate(date.getDate() + 1);
-    if (date.getDay() === 0) {
+    if (date.getDay() === startOfTheWeekDay) {
       month.push(weekOfDates.join(" "));
       weekOfDates = [];
     }
@@ -99,20 +98,19 @@ function getMonth(monthNumeric: number, yearNumeric: number) {
 
 export const App = () => {
   const date = new Date();
-  const [startMonth, setStartMonth] = React.useState<string>(
-    date.getMonth().toString()
-  );
-  const [startYear, setStartYear] = React.useState<string>(
-    date.getFullYear().toString()
-  );
+  const [startMonth, setStartMonth] = React.useState<string>("0");
+  const [startYear, setStartYear] = React.useState<string>(date.getFullYear().toString());
+  const [startDay, setStartDay] = React.useState<"sun"|"mon">("sun");
 
   const onStartMonthChange = (value: string) => setStartMonth(value);
   const onStartYearChange = (value: string) => setStartYear(value);
+  const onStartDayChange = (value: "sun"|"mon") => setStartDay(value);
 
   const onClick = () => {
     const data = getMonth(
       Number.parseInt(startMonth),
-      Number.parseInt(startYear)
+      Number.parseInt(startYear),
+      startDay
     );
     addNativeElement({
       type: "TEXT",
@@ -120,33 +118,54 @@ export const App = () => {
     });
   };
 
+  const options = [
+  {
+    "value": "sun",
+    "label": "Sunday"
+  },
+  {
+    "value": "mon",
+    "label": "Monday"
+  },
+];
+
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
-        <Text>Select the starting and end date for your calendar.</Text>
         <FormField
-          label="Start Month"
-          description="Starting Month"
+          label="Month"
+          description="Which month should the calendar use?"
           control={() => (
             <MonthSelect
-              initialValue={date.getMonth().toString()}
+              initialValue={startMonth}
               onChange={onStartMonthChange}
             />
           )}
         />
         <FormField
-          label="Start Year"
-          description="Starting Year"
+          label="Year"
+          description="Which year should the calendar use?"
           control={() => (
             <YearSelect
-              initialValue={date.getFullYear().toString()}
+              initialValue={startYear}
               onChange={onStartYearChange}
+            />
+          )}
+        />
+        <FormField
+          label="First Day of the Week"
+          description="What is the first day of the week?"
+          control={() => (
+            <RadioGroup
+              options={options}
+              defaultValue={startDay}
+              onChange={onStartDayChange}
             />
           )}
         />
 
         <Button variant="primary" onClick={onClick} stretch>
-          Add Calendar
+          Add Textual Calendar
         </Button>
       </Rows>
     </div>
