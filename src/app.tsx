dbc77568c76de7d14242774bb84d86983bfe3f74
stephenyu <s.yu@less3.com>
2023-11-30 09:12:30 +0000

    Include Heading or Days toggle

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,39 @@
-import { Button, FormField, Select, Rows, RadioGroup } from "@canva/app-ui-kit";
+import {
+  Button,
+  Switch,
+  FormField,
+  Select,
+  Rows,
+  RadioGroup,
+} from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
+import { exit } from "process";
 import * as React from "react";
 import styles from "styles/components.css";
 
 const listOfMonths = (() => {
-// todo: I need to localise this
-return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  // todo: I need to localise this
+  return [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 })();
+
+type CalendarConfig = {
+  includeDaysOfTheWeek: boolean;
+  includeHeading: boolean;
+  startOfTheWeek: "sun" | "mon";
+};
 
 const MonthSelect = ({
   initialValue,
@@ -44,10 +71,14 @@ const YearSelect = ({
 const doubleSpaceNumber = (dateString: string) =>
   dateString.length == 1 ? " " + dateString : dateString;
 
-function getMonth(monthNumeric: number, yearNumeric: number, startDay: "sun"|"mon") {
+function getMonth(
+  monthNumeric: number,
+  yearNumeric: number,
+  calendarConfig: CalendarConfig
+) {
+  const { includeDaysOfTheWeek, includeHeading, startOfTheWeek } =
+    calendarConfig;
   const date = new Date();
-
-  console.log(startDay);
 
   date.setMonth(monthNumeric);
   date.setFullYear(yearNumeric);
@@ -58,27 +89,33 @@ function getMonth(monthNumeric: number, yearNumeric: number, startDay: "sun"|"mo
     year: "numeric",
   } as Intl.DateTimeFormatOptions;
 
-  const monthHeading = new Intl.DateTimeFormat("en-US", options).format(date);
-  let weekOfDates: Array<string> = [];
+  let month: Array<string> = [];
+  if (includeHeading === true)
+    month.push(new Intl.DateTimeFormat("en-US", options).format(date));
 
-  const currentMonth = date.getMonth();
-
-  const month: Array<string> = (startDay === "sun")
-    ? [monthHeading, "Su Mo Tu We Th Fr Sa"]
-    : [monthHeading, "Mo Tu We Th Fr Sa Su"];
-
-  if (startDay === "sun") {
-      for (let i = date.getDay(); i > 0; i--) {
-          weekOfDates.push("  ");
-      }
-  } else {
-      const count = (date.getDay() === 0) ? 6 : date.getDay() - 1;
-      for (let i = 0; i < count; i++) {
-          weekOfDates.push("  ");
-      }
+  if (includeDaysOfTheWeek === true) {
+    if (startOfTheWeek === "sun") {
+      month.push("Su Mo Tu We Th Fr Sa");
+    } else {
+      month.push("Mo Tu We Th Fr Sa Su");
+    }
   }
 
-  const startOfTheWeekDay = (startDay === "sun") ? 0 : 1;
+  let weekOfDates: Array<string> = [];
+
+  if (startOfTheWeek === "sun") {
+    for (let i = date.getDay(); i > 0; i--) {
+      weekOfDates.push("  ");
+    }
+  } else {
+    const count = date.getDay() === 0 ? 6 : date.getDay() - 1;
+    for (let i = 0; i < count; i++) {
+      weekOfDates.push("  ");
+    }
+  }
+
+  const startOfTheWeekDay = startOfTheWeek === "sun" ? 0 : 1;
+  const currentMonth = date.getMonth();
 
   while (date.getMonth() === currentMonth) {
     const numericalDate = date.getDate();
@@ -99,35 +136,61 @@ function getMonth(monthNumeric: number, yearNumeric: number, startDay: "sun"|"mo
 export const App = () => {
   const date = new Date();
   const [startMonth, setStartMonth] = React.useState<string>("0");
-  const [startYear, setStartYear] = React.useState<string>(date.getFullYear().toString());
-  const [startDay, setStartDay] = React.useState<"sun"|"mon">("sun");
+  const [startYear, setStartYear] = React.useState<string>(
+    date.getFullYear().toString()
+  );
+
+  const [calendarConfig, setCalendarConfig] = React.useState<CalendarConfig>({
+    includeDaysOfTheWeek: true,
+    includeHeading: true,
+    startOfTheWeek: "sun",
+  });
 
   const onStartMonthChange = (value: string) => setStartMonth(value);
   const onStartYearChange = (value: string) => setStartYear(value);
-  const onStartDayChange = (value: "sun"|"mon") => setStartDay(value);
+  const onStartDayChange = (value: "sun" | "mon") =>
+    setCalendarConfig((prevState) => ({
+      ...prevState,
+      startOfTheWeek: value,
+    }));
+
+  const onIncludeHeadingChange = (value: boolean) =>
+    setCalendarConfig((prevState) => ({
+      ...prevState,
+      includeHeading: value,
+    }));
+
+  const onIncludeDaysOfWeekChange = (value: boolean) =>
+    setCalendarConfig((prevState) => ({
+      ...prevState,
+      includeDaysOfTheWeek: value,
+    }));
 
   const onClick = () => {
     const data = getMonth(
       Number.parseInt(startMonth),
       Number.parseInt(startYear),
-      startDay
+      calendarConfig
     );
-    addNativeElement({
-      type: "TEXT",
-      children: [data.join("\n")],
-    });
+
+    if (data !== undefined) {
+      addNativeElement({
+        type: "TEXT",
+        children: [data.join("\n")],
+      });
+    }
   };
 
   const options = [
-  {
-    "value": "sun",
-    "label": "Sunday"
-  },
-  {
-    "value": "mon",
-    "label": "Monday"
-  },
-];
+    {
+      value: "sun",
+      label: "Sunday",
+    },
+    {
+      value: "mon",
+      label: "Monday",
+    },
+  ];
 
   return (
     <div className={styles.scrollContainer}>
@@ -146,10 +209,7 @@ export const App = () => {
           label="Year"
           description="Which year should the calendar use?"
           control={() => (
-            <YearSelect
-              initialValue={startYear}
-              onChange={onStartYearChange}
-            />
+            <YearSelect initialValue={startYear} onChange={onStartYearChange} />
           )}
         />
         <FormField
@@ -158,14 +218,26 @@ export const App = () => {
           control={() => (
             <RadioGroup
               options={options}
-              defaultValue={startDay}
+              defaultValue={calendarConfig.startOfTheWeek}
               onChange={onStartDayChange}
+              disabled={calendarConfig.includeDaysOfTheWeek === false}
             />
           )}
         />
+        <Switch
+          label="Include Month and Year Heading?"
+          value={calendarConfig.includeHeading}
+          onChange={onIncludeHeadingChange}
+        />
+
+        <Switch
+          label="Include Days of the Week?"
+          value={calendarConfig.includeDaysOfTheWeek}
+          onChange={onIncludeDaysOfWeekChange}
+        />
 
         <Button variant="primary" onClick={onClick} stretch>
-          Add Textual Calendar
+          Add Calendar
         </Button>
       </Rows>
     </div>
