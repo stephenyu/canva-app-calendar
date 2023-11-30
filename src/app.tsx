e619b1ccc894672e8b4c259225f755466c5eafd8
stephenyu <s.yu@less3.com>
2023-11-30 10:16:05 +0000

    Added Calendar Test

--- src/app.tsx
+++ src/app.tsx
@@ -7,9 +7,15 @@ import {
   RadioGroup,
 } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
-import { exit } from "process";
 import * as React from "react";
 import styles from "styles/components.css";
+import { getMonth } from './calendarUtils';
+
+export interface CalendarConfig {
+  includeDaysOfTheWeek: boolean;
+  includeHeading: boolean;
+  startOfTheWeek: "sun" | "mon";
+};
 
 const listOfMonths = (() => {
   // todo: I need to localise this
@@ -29,11 +35,6 @@ const listOfMonths = (() => {
   ];
 })();
 
-type CalendarConfig = {
-  includeDaysOfTheWeek: boolean;
-  includeHeading: boolean;
-  startOfTheWeek: "sun" | "mon";
-};
 
 const MonthSelect = ({
   initialValue,
@@ -67,71 +68,6 @@ const YearSelect = ({
 
   return <Select value={initialValue} options={options} onChange={onChange} />;
 };
-
-const doubleSpaceNumber = (dateString: string) =>
-  dateString.length == 1 ? " " + dateString : dateString;
-
-function getMonth(
-  monthNumeric: number,
-  yearNumeric: number,
-  calendarConfig: CalendarConfig
-) {
-  const { includeDaysOfTheWeek, includeHeading, startOfTheWeek } =
-    calendarConfig;
-  const date = new Date();
-
-  date.setMonth(monthNumeric);
-  date.setFullYear(yearNumeric);
-  date.setDate(1);
-
-  const options = {
-    month: "long",
-    year: "numeric",
-  } as Intl.DateTimeFormatOptions;
-
-  let month: Array<string> = [];
-  if (includeHeading === true)
-    month.push(new Intl.DateTimeFormat("en-US", options).format(date));
-
-  if (includeDaysOfTheWeek === true) {
-    if (startOfTheWeek === "sun") {
-      month.push("Su Mo Tu We Th Fr Sa");
-    } else {
-      month.push("Mo Tu We Th Fr Sa Su");
-    }
-  }
-
-  let weekOfDates: Array<string> = [];
-
-  if (startOfTheWeek === "sun") {
-    for (let i = date.getDay(); i > 0; i--) {
-      weekOfDates.push("  ");
-    }
-  } else {
-    const count = date.getDay() === 0 ? 6 : date.getDay() - 1;
-    for (let i = 0; i < count; i++) {
-      weekOfDates.push("  ");
-    }
-  }
-
-  const startOfTheWeekDay = startOfTheWeek === "sun" ? 0 : 1;
-  const currentMonth = date.getMonth();
-
-  while (date.getMonth() === currentMonth) {
-    const numericalDate = date.getDate();
-    weekOfDates.push(doubleSpaceNumber(numericalDate.toString()));
-
-    date.setDate(date.getDate() + 1);
-    if (date.getDay() === startOfTheWeekDay) {
-      month.push(weekOfDates.join(" "));
-      weekOfDates = [];
-    }
-  }
-
-  month.push(weekOfDates.join(" "));
-
-  return month;
-}
 
 export const App = () => {
   const date = new Date();
